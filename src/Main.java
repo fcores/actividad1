27258c8544c097a40963aae8cf0355e5f0748e9e
fcores <facundo.cores@gmail.com>
2025-01-28 00:20:13 -0300

    actividad4 fin

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        //PEDIR UN CALOR
+        System.out.println("Solicitar Numero 1");
+        int num1 = scanner.nextInt();
+
+        for (int i = 1; i<=num1; i++){
+            if(i % 2 == 0){
+                System.out.println("El número: " + i + " es Par");
+            }else{
+                System.out.println("El número: " + i + " es Impar");
+            }
+        }
+
+        scanner.close();
     }
 }
